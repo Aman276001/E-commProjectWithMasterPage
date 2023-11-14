1b561167cf5f2cd7d33a2027cb14c02e16ce6323
Aman kumar <119146542+Aman276001@users.noreply.github.com>
2023-11-14 12:15:55 +0530

    thanks.aspx.cs

--- E-commProjectWithMasterPage/User/thanks.aspx.cs
+++ E-commProjectWithMasterPage/User/thanks.aspx.cs
@@ -1,17 +1,46 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
 using System.Linq;
 using System.Web;
+using System.Web.Configuration;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Diagnostics;
+using System.Reflection.Emit;
 
 namespace E_commProjectWithMasterPage.User
 {
     public partial class thanks : System.Web.UI.Page
     {
+        SqlConnection conn = new SqlConnection(WebConfigurationManager.ConnectionStrings["constr"].ToString());
+        string query, userData;
+        SqlDataAdapter adp;
+        DataTable dt = new DataTable();
+        static string userID;
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (Request.QueryString["user"] != null)
+            {
+                userID = Request.QueryString["user"];
 
+                if (!IsPostBack)
+                {
+                   
+
+                }
+            }
+            else
+            {
+                Response.Redirect("userLogin.aspx");
+            }
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("HomeUser.aspx?user=" + userID + "");
         }
     }
 }
